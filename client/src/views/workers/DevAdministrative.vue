b3d10a88019215a977d91ae4dd514c1365e9ab67
fin <1240814287@qq.com>
2019-07-06 15:25:26 +0800

    顾:完成学院人员板块

--- client/src/views/workers/DevAdministrative.vue
+++ client/src/views/workers/DevAdministrative.vue
@@ -1,71 +1,174 @@
 ﻿<template>
         <div class="content">
             <dev-layout>
-                    <Card :bordered="ture">
-                        <img src="./NO.1.png"/>
-                        <p slot="title">No border title</p>
-                        <p>Content of no border type. Content of no border type. Content of no border type. Content of no border type. </p>
-                    </Card>
+                <h2 style="margin-left:20px;">行政人员</h2>
+                <br />
+                    <Row :gutter="30">
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">许道云</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">贵州大学计算机科学与技术学院院长 / 教授 / 博士生导师 / 专家委员会主任</p>
+                                <p>邮箱:juancheng2016@qq.com</p>
+                            </Card>
+                        </div>
+                        </Col>
 
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">秦永斌</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">贵州大学计算机科学与技术学院副院长 / 博士 / 副教授 / 专家委员会副主任</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
 
-                    <h2 align="center">行政人员</h2>
-                    <br />
-                    <p style="text-indent:2em">
-                        许道云（贵州大学计算机科学与技术学院院长 / 教授 / 博士生导师 / 专家委员会主任）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        秦永斌（贵州大学计算机科学与技术学院副院长 / 博士 / 副教授 / 专家委员会副主任）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        陈滢（博士 / 慧科研究院院长 / 原IBM中国研究院副院长 / 专家委员会副主任）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        姜旭平（清华大学教授 / 互联网营销奠基人）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        林仕鼎（原百度大数据首席架构师）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        欧延亮（硕士 / 原Adobe中国研发中心架构师）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        方业昌（美国FIU博士 / 慧科集团创始人）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        岳喜伟（清华大学MBA / 慧科集团CEO）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        王浩（清华大学博士 / 原IBM中国大学合作部总经理）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        管刚（清华大学博士 / 原腾讯教育事业部总经理）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        李秦伟（贵州大学计算机科学与技术学院副院长 / 教授）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        龙慧云（贵州大学计算机科学与技术学院副院长 / 博士 / 副教授）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        李智（博士 / 副教授）
-                    </p>
-                    <br />
-                    <p style="text-indent:2em">
-                        彭长根（博士 / 教授）
-                    </p>
-                    <br />
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">陈滢</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">贵州大学计算机科学与技术学院院长 / 教授 / 博士生导师 / 专家委员会主任</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+                    </Row>
+                    
+                    <Row :gutter="30">
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">姜旭平</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">清华大学教授 / 互联网营销奠基人</p>
+                                <p>邮箱:juancheng2016@qq.com</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">林仕鼎</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">原百度大数据首席架构师</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">欧延亮</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">硕士 / 原Adobe中国研发中心架构师</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+                    </Row>
+
+                    <Row :gutter="30">
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">方业昌</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">美国FIU博士 / 慧科集团创始人</p>
+                                <p>邮箱:juancheng2016@qq.com</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">岳喜伟</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">清华大学MBA / 慧科集团CEO</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">王浩</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">清华大学博士 / 原IBM中国大学合作部总经理</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+                    </Row>
+
+                    <Row :gutter="30">
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">管刚</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">清华大学博士 / 原腾讯教育事业部总经理</p>
+                                <p>邮箱:juancheng2016@qq.com</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">李秦伟</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">贵州大学计算机科学与技术学院副院长 / 教授</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">龙慧云</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">贵州大学计算机科学与技术学院副院长 / 博士 / 副教授</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+                    </Row>    
+
+                    <Row :gutter="30">
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">李智</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">博士 / 副教授</p>
+                                <p>邮箱:juancheng2016@qq.com</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        <Col span="7">
+                        <div style="padding: 20px">
+                            <Card :bordered="false">
+                                <p slot="title" style="height:160px;" class="name">彭长根</p>
+                                <p slot="extra"><img src="./NO.1.png" alt="放图片" /></p>
+                                <p slot="extra" class="inf">博士 / 教授</p>
+                                <p>邮箱:</p>
+                            </Card>
+                        </div>
+                        </Col>
+
+                        
+                    </Row>
+
             </dev-layout>
         </div>
 </template>
@@ -84,6 +187,27 @@
 <style scoped lang="less">
 
 
+    .name{
+        font-size:20px;
+        color:#2d8cf0;
+        padding-top:20px;
+        padding-left:30px;
+        width:150px;
+        margin-bottom:20px;
+    }
+    .inf{
+        
+        margin-top:5px;
+        margin-left:5px;
+        width:200px;
+    }
+    img{
+        margin-left:125px;
+        width:70px;
+        height:70px;
+        border-radius:70px;
+        border: 2px solid #2d8cf0;
+    }
     p {
         line-height: 30px;
         font-size: 16px;
